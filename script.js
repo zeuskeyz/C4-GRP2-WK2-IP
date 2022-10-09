4e4b79341c84247cf4f2f777a52d11e8287b6353
zeus <zeuskeyz@gmail.com>
2022-10-09 12:52:51 +0300

    adds validations to the code

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+const submitButton = document.querySelector(".subMitter");
+
 function myAkanName() {
-    
-    var birthDateInput = document.getElementById("dateOfBirth"); 
+    /*OUR WORKING LISTS */
+    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; //list of the days of week
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //list of female Akan names
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; //list of male Akan names
+
+
+    /*DATE MANIPULATION VARIABLES*/
+    var birthDateInput = document.getElementById("dateOfBirth");//uses the variable dateInput from the user prompt to create a calendar date
+    var birthDateDetails = new Date(birthDateInput.value);
+    var birthDay = birthDateDetails.getDay(); // picks the day from the date object
+    var birthMonth = birthDateDetails.getMonth();// picks the month from the dabject
+    var birthYear = birthDateDetails.getFullYear(); //picks the month from the dabject
     var genderInput = document.getElementById("genderChoice");
     let textResult = document.getElementById("feedBack");
+    /* USING THE DATES TO ACCESS THE NAME LISTS*/
+    var genderFemale = femaleNames[birthDay];// uses the day of birth as index to access the femalesNames
+    var genderMale = maleNames[birthDay]; //uses the day of birth as index to access the femalesNames
 
-    var birthDateDetails = new Date(birthDateInput.value); //CREATES A NEW DATE OBJECT USING THE DETAILS INPUT BY THE USER STARTING WIT YEAR MONTH DATE HOUR MINUTE etc
-    var birthDay = birthDateDetails.getDay();//THE getDay() METHOD PICKS THE DAY FROM THE DATE IN FORM OF A NUMBER FROM 0-6 SUN-SAT
-    
-    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; // WE DECLARE AND INITIALISE AN ARRAY WITH THE DAYS OF THE WEEK STARTING FROM SUNDAY TO MONDAY
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //ARRAY OF FEMALE AKAN NAMES
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; //ARRAY OF MALE AKAN NAMES
-
-
+    /*CAPTURE DATE INPUTS AND CHECKS IF ITS VALID*/
+    if (birthDay >= 0 && birthDay < 7 && birthMonth >= 0 && birthMonth < 12 && birthYear >= 1000) {
+        
         if (genderInput.value === "Male" || genderInput.value === "Female") {
 
             if (genderInput.value === "Male") {
@@ -30,4 +40,15 @@ function myAkanName() {
             }
 
         }
+
+        
+    }
+
+    else {
+        alert("WRONG DATE FORMAT. PLEASE USE YYYY-MM-DD\nExample: 2022-10-08");//triggers error alert if user date input is invalid
+    }
+
+
 }
+
+submitButton.addEventListener("click", myAkanName);
