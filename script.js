45fe9f9fde1e67b85b07036b231af418cc40cd54
zeus <zeuskeyz@gmail.com>
2022-10-11 07:46:51 +0300

    final changes to the fonts and colors and output styles

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const submitButton = document.querySelector(".subMitter");
 const wiperButton = document.querySelector("#theWiper")
 
 function myAkanName() {
+
     /*OUR WORKING LISTS */
     var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; //list of the days of week
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //list of female Akan names
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; //list of male Akan names
-
 
     /*DATE MANIPULATION VARIABLES*/
     var birthDateInput = document.getElementById("dateOfBirth");//uses the variable dateInput from the user prompt to create a calendar date
@@ -16,6 +16,7 @@ function myAkanName() {
     var birthYear = birthDateDetails.getFullYear(); //picks the month from the dabject
     var genderInput = document.getElementById("genderChoice");
     let textResult = document.getElementById("feedBack");
+
     /* USING THE DATES TO ACCESS THE NAME LISTS*/
     var genderFemale = femaleNames[birthDay];// uses the day of birth as index to access the femalesNames
     var genderMale = maleNames[birthDay]; //uses the day of birth as index to access the femalesNames
@@ -27,17 +28,18 @@ function myAkanName() {
 
             if (genderInput.value === "Male") {
 
+                textResult.innerHTML = "Your Akan name is " + maleNames[birthDay] + " because you are "+ genderInput.value + " born on "+ daysOfWeek[birthDay]+"!";
                 birthDateInput.value = '';
                 genderInput.value=genderInput[0].innerHTML;
-                textResult.innerHTML = maleNames[birthDay];
+                
                 
             }
 
             else {
-
+                textResult.innerHTML = "Your Akan name is " + femaleNames[birthDay] + " because you are "+ genderInput.value + " born on "+ daysOfWeek[birthDay]+"!";
                 birthDateInput.value = '';
                 genderInput.value = genderInput[0].innerHTML;
-                textResult.innerHTML = femaleNames[birthDay] ;
+                
             }
 
         }
